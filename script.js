d9288f404c9718e55ea8e5b0792a8801f843f740
icollectmugs <150671713+icollectmugs@users.noreply.github.com>
2024-08-26 17:55:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Changing Jumbotron Image
+function changeImage(element) {
+    element.setAttribute("src", "assets/picture4.png");
+    // element.setAttribute("height", "300")
+    element.setAttribute("class", "jumbotron-img2")
+}
+    function changeImageBack(element) {
+    element.setAttribute("src", "assets/picture2.png");
+    // element.setAttribute("height", "300")
+}
