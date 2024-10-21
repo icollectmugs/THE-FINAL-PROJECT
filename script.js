d7da82fdd044e12a985f65470e48c512b58788ba
icollectmugs <150671713+icollectmugs@users.noreply.github.com>
2024-10-21 17:21:22 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ function changeImage(element) {
 }
 
 AOS.init();
+
+// Form
+var btnSubmit = document.querySelector('.submit');
+var modal = document.querySelector('.modal-container')
+
+if(btnSubmit){
+    btnSubmit.addEventListener('click', function(){
+        modal.classList.add('show');
+
+        const next = document.querySelector('.next');
+        next.setAttribute('value', 'https://cobee-the-junior-programmer.netlify.app/contact.html');
+    })
+}
