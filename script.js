c47577151a42feb178264700bfa4c3bc4a7662ac
icollectmugs <150671713+icollectmugs@users.noreply.github.com>
2024-10-23 16:59:23 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,55 @@
 // Changing Jumbotron Image
 function changeImage(element) {
-    element.setAttribute("src", "assets/picture4.png");
-    // element.setAttribute("height", "300")
-    element.setAttribute("class", "jumbotron-img2")
+  element.setAttribute("src", "assets/picture4.png");
+  // element.setAttribute("height", "300")
+  element.setAttribute("class", "jumbotron-img2");
 }
-    function changeImageBack(element) {
-    element.setAttribute("src", "assets/picture2.png");
-    // element.setAttribute("height", "300")
+function changeImageBack(element) {
+  element.setAttribute("src", "assets/picture2.png");
+  // element.setAttribute("height", "300")
 }
-
-
 
 // Form
-var btnSubmit = document.querySelector('.submit');
-console.log(btnSubmit)
-var modal = document.querySelector('.modal-container')
+var btnSubmit = document.querySelector(".submit");
+console.log(btnSubmit);
+var modal = document.querySelector(".modal-container");
 
-if(btnSubmit){
-    btnSubmit.addEventListener('click', function(){
-        modal.classList.add('show');
+if (btnSubmit) {
+  btnSubmit.addEventListener("click", function () {
+    modal.classList.add("show");
 
-        const next = document.querySelector('.next');
-        next.setAttribute('value', 'https://cobee-the-junior-programmer.netlify.app/contact.html');
-    })
+    const next = document.querySelector(".next");
+    next.setAttribute(
+      "value",
+      "https://cobee-the-junior-programmer.netlify.app/contact.html"
+    );
+  });
 }
 
 AOS.init();
+
+// Splash
+var splash = document.querySelector(".splash");
+if (splash) {
+  document.addEventListener("DOMContentLoaded", function (event) {
+    setTimeout(function () {
+      splash.style.display = "none";
+    }, 3000);
+  });
+}
+
+var animate = document.querySelector(".astronaut-takeoff");
+if (animate) {
+  document.addEventListener("DOMContentLoaded", function (event) {
+    setTimeout(function () {
+      animate.classList.add("animation");
+    }, 400);
+  });
+}
+
+var splashText = document.querySelector(".splash-text");
+if (splashText) {
+  setTimeout(function () {
+    splashText.innerHTML = "Come in!";
+  }, 2000);
+}
